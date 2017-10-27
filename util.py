ffd4612d51c7e96f1adc16498296bd3d281ad33b
Nash Kaminski <homeassistant@skynet.home>
2017-10-27 14:52:27 -0500

    Add debug options

--- util.py
+++ util.py
@@ -3,6 +3,5 @@ from config import *
 
 def publish_mqtt(topic, payload):
     publish.single(topic, payload, hostname=MQTT_HOST, auth={'username': MQTT_USER, 'password': MQTT_PASS})
-    print("%s -> %s" % (topic,payload))
-
-
+    if(DEBUG):
+        print("%s -> %s" % (topic,payload))
